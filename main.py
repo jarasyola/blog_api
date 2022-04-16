f00d1870da86f494aa291679d0f5490d05ac5f55
jarasyola <jamraph10@gmail.com>
2022-04-16 12:11:59 +0300

    Break it down and understanding

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
+# where @app is  Routes/Endpoint/Decorator/Path(Python)
+# .get/post/put - operation
+# def index - function operation
 @app.get('/')
 def index():
     return {"data":{"name": "James"}}
