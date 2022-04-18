4e5caf8216ebfbbab1bb2fbcd4e552690cdc7e9b
jarasyola <jamraph10@gmail.com>
2022-04-18 14:56:26 +0300

    Create User

--- blog/schemas.py
+++ blog/schemas.py
@@ -2,11 +2,18 @@ from pydantic import BaseModel
 
 class Blog(BaseModel):
     title: str
-    body: str 
+    body: str
 
 class ShowBlog(Blog):
     title: str
-    body: str 
-    
+    body: str
+
     class Config():
         orm_mode = True
+
+
+class User(BaseModel):
+    name: str
+    email: str 
+    password: str
+
