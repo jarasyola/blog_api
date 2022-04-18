f463dc7e35a42486a8dfeb506038eb6c59a8e26f
jarasyola <jamraph10@gmail.com>
2022-04-18 16:35:31 +0300

    Relationship

--- blog/main.py
+++ blog/main.py
@@ -8,6 +8,7 @@ from . hashing import Hash
 
 
 
+
 #Line for creating the database tables
 models.Base.metadata.create_all(bind=engine)
 
@@ -25,7 +26,7 @@ def get_db():
 # Creating blog
 @app.post('/blog',status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title,body=request.body)
+    new_blog = models.Blog(title=request.title,body=request.body,user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel
+from typing import List
 
 class Blog(BaseModel):
     title: str
     body: str
 
-class ShowBlog(Blog):
-    title: str
-    body: str
-
     class Config():
         orm_mode = True
+
+
 
 
 class User(BaseModel):
@@ -20,7 +19,16 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     name: str
     email: str
-    
+    blogs: List[Blog]=[]
+
+    class Config():
+        orm_mode = True
+
+class ShowBlog(Blog):
+    title: str
+    body: str
+    creator:ShowUser
+
     class Config():
         orm_mode = True
     
