1eebbecbf11075e9f316ba83be06bbe862ff1940
jarasyola <jamraph10@gmail.com>
2022-04-18 12:57:53 +0300

    Updating a blog

--- blog/main.py
+++ blog/main.py
@@ -35,6 +35,18 @@ def destroy(id, db: Session = Depends(get_db)):
     return 'Blog is deleted successfully'
 
 
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id,request:schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail =f'Blog with id {id} is not found')
+
+    blog.update({'title':'updated title','body':'updated body'})
+    
+    db.commit()
+    return 'Updated Successfully.'
 
 
 # Search/returning all blogs
