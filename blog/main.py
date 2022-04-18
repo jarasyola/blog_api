b5e8505610c3675fb3bbae31bf9966cdb9c2b9d3
jarasyola <jamraph10@gmail.com>
2022-04-18 12:13:25 +0300

    Delete a blog

--- blog/main.py
+++ blog/main.py
@@ -28,6 +28,15 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    db.commit()
+    return 'Blog is deleted successfully'
+
+
+
+
 # Search/returning all blogs
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
