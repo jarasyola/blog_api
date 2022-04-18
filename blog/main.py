2d8bee89ab59bcee7d06462cf731fc75783f8dd0
jarasyola <jamraph10@gmail.com>
2022-04-18 15:27:10 +0300

    Hash Password

--- blog/main.py
+++ blog/main.py
@@ -2,7 +2,9 @@ from typing import List
 from fastapi import FastAPI,Depends,status,Response,HTTPException
 from . import schemas,models
 from . database import engine,SessionLocal
-from sqlalchemy.orm import Session 
+from sqlalchemy.orm import Session
+from . hashing import Hash
+
 
 
 
@@ -69,10 +71,11 @@ def show(id,response: Response, db: Session = Depends(get_db)):
     return blog
 
 
+
 # Users
 @app.post('/user')
 def create_user(request: schemas.User,db: Session = Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
